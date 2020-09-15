ad322963effece07c083088bddc0b7908c0ff012
Joao Victtor S Melo <joaovicttorsilveiramelo@gmail.com>
2020-09-15 15:13:53 -0300

    create project

---
+++ backend/src/test/java/com/dvsuperior/dspesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvsuperior.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
