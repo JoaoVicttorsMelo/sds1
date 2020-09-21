dc87719e1609c80feb4cfe277cb0fc417163bb15
Joao Victtor S Melo <joaovicttorsilveiramelo@gmail.com>
2020-09-21 00:56:07 -0300

    arrumando erro

--- front-web/src/src/components/Header/index.tsx
+++ front-web/src/src/components/Header/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { Text } from 'react-native';
-
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+import './style.css'
 const Header = () => {
     return (
-
+        <header className="main-header">
+            <Logo />
+            <div className="logo-text1">
+                Big Game
+            </div>
+            <div className="logo-text2">
+                Survey
+                </div>
+        </header>
     );
 }
+export default Header;
